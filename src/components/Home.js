ff090bd98e2c48a7a462a9b289f1cc2ac13b206e
Rafia Khandaker <rafiakhandaker@gmail.com>
2022-08-29 16:05:31 -0400

    commit tree d2d675ca103d6731ab3825d4d12d78bb0e9ed983

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,14 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  const h1Color = {
+    color: 'firebrick'
+  };
+  return (
+  <div>Home
+  <h1 style = {h1Color}>Your {name} is a Web Developer from Your {city}</h1>
+  </div>
+  )
 }
 
 export default Home;
